0364988a195df230897a8eb2066ce4ba017c80e5
2004667 <220103109@stu.sdu.edu.kz>
2025-04-11 19:01:22 +0500

    assign19

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
 const express = require('express');
 const axios = require('axios');
-
 const app = express();
 const port = 4040;
 
-app.get('/user/:id', async (req, res) => {
-    const { id } = req.params;
+app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/users', async (req, res) => {
     try {
         const response = await axios.get('https://api.slingacademy.com/v1/sample-data/users');
         const users = response.data.users;
-        if (!users || !users[id]) {
-            throw new Error(`Пользователь с ID ${id} не найден`);
-        }
-
-        const user = users[id];
-        const result = {
-            name: `${user.first_name} ${user.last_name}`,
-            email: user.email,
-            country: user.country,
-            job: user.job,
-        };
-
-        res.json(result);
-
+        res.render('users', { users });
     } catch (error) {
-        res.status(500).json({
-            error: error.message || 'Произошла ошибка при получении данных',
-        });
+        res.status(500).send('Ошибка при получении данных пользователей');
     }
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
 });
 
 app.listen(port, () => {
